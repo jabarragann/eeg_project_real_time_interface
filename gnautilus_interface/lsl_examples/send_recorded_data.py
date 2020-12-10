2cfb4a1f6efc743dc682a4f0e8d326f760644cb2
Juan Antonio Barragan <barragan@purdue.edu>
2020-12-09 23:45:58 -0500

    Send recorded data LSL script

---
+++ gnautilus_interface/lsl_examples/send_recorded_data.py
@@ -0,0 +1,49 @@
+import time
+import numpy as np
+import mne
+from pylsl import StreamInfo, StreamOutlet
+from pathlib import Path
+
+def renameChannels(chName):
+    if 'Z' in chName:
+        chName = chName.replace('Z','z')
+    if 'P' in chName and 'F' in chName:
+        chName = chName.replace('P','p')
+    return chName
+
+
+# # next make an outlet
+# channelsSubset = ['FZ', 'F7', 'F3', 'F4', 'F8', 'T8','T7','P7','P8','OZ']
+# info = StreamInfo('Gnautilus', 'EEG',10, 250, 'float32', 'myuid34234')
+# outlet = StreamOutlet(info)
+
+
+
+if __name__ == '__main__':
+
+    #Read eeg file
+    srcPath = Path('./../deep_models/raw-data/').resolve()
+    file = srcPath / "Juan_S04_T01_Baseline_raw.edf"
+    #Load data
+    raw = mne.io.read_raw_edf(file, preload=True)
+    rawArray = raw.get_data(picks=['eeg'])
+
+    # Create LSL outlet
+    info = StreamInfo("Gnautilus recording", 'eeg', 32, 250, 'float32', "gnaut.recorded")
+    # Append channel meta-data
+    info.desc().append_child_value("manufacturer", "G.tec")
+    channels = info.desc().append_child("channels")
+    for c in raw.ch_names:
+        channels.append_child("channel") \
+            .append_child_value("label", c) \
+            .append_child_value("unit", "microvolts") \
+            .append_child_value("type", "EEG")
+
+    outlet = StreamOutlet(info)
+    print("Sending data ...")
+    for idx in range(rawArray.shape[1]):
+        # now send it and wait for a bit
+        outlet.push_sample(rawArray[:,idx])
+        time.sleep(0.004)
+
+    print("Finished sending data")
